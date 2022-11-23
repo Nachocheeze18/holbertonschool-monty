67c977488f9ba9b7ce56d6f5d151467db1f966a7
Nachocheeze18 <5581@holbertonstudents.com>
2022-11-22 20:40:44 -0800

    new file

--- monty.c
+++ monty.c
@@ -1,1 +1,34 @@
+#include "monty.h"
 
+/**
+ * 
+ */
+
+void push(stack_t stack, int i)
+{
+
+int val = atoi(i);
+
+if (val = 0 || i == NULL)
+{
+fprintf(stderr, "L<%d>: usage: push integer\n", __LINE__);
+exit(EXIT_FAILURE);
+}
+}
+
+/**
+ *
+ */
+
+void pall(stack_t stack, int i)
+{
+
+int val = atoi(i);
+
+if (val = 0 || i == NULL)
+{
+fprintf(stderr, "l<%d>: usage: pall integer\n", __LINE__);
+exit(EXIT_FAILURE);
+}
+
+}
