86e6633b016a2a00f76e0379dc3167c5a1818cb3
Nachocheeze18 <5581@holbertonstudents.com>
2022-12-03 17:13:15 -0800

    17th attempt

--- monty.h
+++ monty.h
@@ -1,25 +1,16 @@
-#ifndef montyh
-#define montyh
-#undef montyh
+#ifndef _MONTY
+#define _MONTY
 
-#include <stdlib.h>
+/* Libraries/Headers */
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/types.h>
 #include <string.h>
 
-/**
- * struct check
- * @check1: var
- *@check2: var
- * Description: token struct
- */
-typedef struct checker
-{
-char *check1;
-char *check2;
-FILE *file;
-} checker;
-
-extern checker *c;
+int main(int argc, char *argv[]);
 
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
@@ -27,7 +18,7 @@ extern checker *c;
  * @prev: points to the previous element of the stack (or queue)
  * @next: points to the next element of the stack (or queue)
  * Description: doubly linked list node structure
- * for stack, queues, LIFO, FIFO
+ * for stack, queues, LIFO, FIFO Holberton project
  */
 typedef struct stack_s
 {
@@ -35,14 +26,13 @@ int n;
 struct stack_s *prev;
 struct stack_s *next;
 } stack_t;
-
 /**
  * struct instruction_s - opcode and its function
  * @opcode: the opcode
  * @f: function to handle the opcode
  *
  * Description: opcode and its function
- * for stack, queues, LIFO, FIFO
+ * for stack, queues, LIFO, FIFO Holberton project
  */
 typedef struct instruction_s
 {
@@ -50,22 +40,39 @@ char *opcode;
 void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
-/**
- * Prototypes - functions
- * main - main function
- * @argcount: how many args
- * @argcont: contents of arg
- */
+int _iswhitespace(void);
+void exit_free(stack_t *stack);
+void free_stack(stack_t *stack);
+void opcomp(stack_t **stack, unsigned int line_number, char *opcode);
+void push(stack_t **stack, unsigned int line_number);
+void pall(stack_t **stack, unsigned int line_number);
+void pint(stack_t **stack, unsigned int line_number);
+void pop(stack_t **stack, unsigned int line_number);
+void swap(stack_t **stack, unsigned int line_number);
+void add(stack_t **stack, unsigned int line_number);
+void sub(stack_t **stack, unsigned int line_number);
+void mul(stack_t **stack, unsigned int line_number);
+void divide(stack_t **stack, unsigned int line_number);
+void mod(stack_t **stack, unsigned int line_number);
+void nop(stack_t **stack, unsigned int line_number);
+void pchar(stack_t **stack, unsigned int line_number);
+void pstr(stack_t **stack, unsigned int line_number);
+void rotl(stack_t **stack, unsigned int line_number);
+void rotr(stack_t **stack, unsigned int line_number);
 
-int main(int argcount, char* argcont);
-void push(stack_t **stack, unsigned int i);
-void pall(stack_t **stack, unsigned int i);
-void pint(stack_t **stack, unsigned int i);
-void pop(stack_t **stack, unsigned int i);
-void swap(stack_t **stack, unsigned int i);
-void add(stack_t **stack, unsigned int i);
-void nop(stack_t **stack, unsigned int i);
-void freemem(stack_t **stack);
-void other(unsigned int linecount, char *ch, stack_t **stack);
+
+
+/**
+ * struct GlobalStruct - contains all globals
+ * @data: int for push
+ * @fm: file stream for monty file
+ * @lineptr: pointer to char sting allocated by getline function
+ */
+struct GlobalStruct
+{
+int data;
+FILE *fm;
+char *lineptr;
+} globes;
 
 #endif
