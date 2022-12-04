0ac6bc5400857ea76fc864555a55dcdbb0cab927
Nachocheeze18 <5581@holbertonstudents.com>
2022-12-04 11:01:11 -0800

    27th attempt

--- monty.h
+++ monty.h
@@ -35,28 +35,29 @@ extern checker *c;
  */
 typedef struct stack_s
 {
-int n;
-struct stack_s *prev;
-struct stack_s *next;
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
 } stack_t;
 
 /**
  * struct instruction_s - opcode and its function
  * @opcode: the opcode
  * @f: function to handle the opcode
+ *
  * Description: opcode and its function
  * for stack, queues, LIFO, FIFO
  */
 typedef struct instruction_s
 {
-char *opcode;
-void (*f)(stack_t **stack, unsigned int line_number);
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
 /**
  * Prototypes
  */
-
+int main(int argcount, char* argcont[])
 void push(stack_t **stack, unsigned int i);
 void pall(stack_t **stack, unsigned int i);
 void pint(stack_t **stack, unsigned int i);
